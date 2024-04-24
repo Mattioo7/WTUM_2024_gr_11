675c7faaa8e9900e1fac171513ad9dcac9a5bed9
unknown <mdp.ch3@gmail.com>
2024-04-24 17:14:29 +0200

    First good training done

--- source/train.py
+++ source/train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 
 def main():
     model = YOLO("yolov8n.yaml")  # build a new model from scratch
-    results = model.train(data="config.yaml", epochs=1)  # train the model
+    results = model.train(data="config.yaml", epochs=8)  # train the model
 
 if __name__ == '__main__':
     torch.multiprocessing.freeze_support()  # Dodaj tę linię
