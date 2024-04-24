8cc02eb0f04ed28dd9bb48f289f9d182a4354f8a
unknown <mdp.ch3@gmail.com>
2024-04-24 12:56:01 +0200

    11 epochs trained

--- source/train.py
+++ source/train.py
@@ -2,8 +2,9 @@ import torch
 from ultralytics import YOLO
 
 def main():
-    model = YOLO("yolov8n.yaml")  # build a new model from scratch
-    results = model.train(data="config.yaml", epochs=1)  # train the model
+    model = YOLO("yolov8n.pt")  
+    #results = model.train(data="config.yaml", epochs=11)  # train the model
+    results = model("35.jpg")
 
 if __name__ == '__main__':
     torch.multiprocessing.freeze_support()  # Dodaj tę linię
